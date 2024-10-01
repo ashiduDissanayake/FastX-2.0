dbd022b395f6efcf4fbe4da4aa915d4af7fc9533
lihini-2002 <lihinikaduarachchilagh.22@uom.lk>
2024-10-01 18:37:38 +0530

    Sales completed

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React from "react";
+import Sales from "../components/home/Sales";
 
 export default function Home() {
   return (
-    <div>Home</div>
-  )
+    <div>
+      <Sales />
+    </div>
+  );
 }

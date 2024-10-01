789f03d2d9c598cfeabe4c819b30e5c05a760255
Ashidu Dissanayake <157984013+ashiduDissanayake@users.noreply.github.com>
2024-10-01 20:33:23 +0530

    .....

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React from "react";
 
 export default function Home() {
   return (
-    <div>Home</div>
-  )
+    <div>
+      <h1 className="text-center text-black text-4xl text-bold">Home</h1>
+    </div>
+  );
 }

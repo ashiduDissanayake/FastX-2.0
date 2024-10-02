93685d3919703f361379d756307a3e5e510082df
Kasun333 <kumarajwkd.22@uom.lk>
2024-10-02 16:19:20 +0530

    Made a carousel
    Made an carousel component inside component folde

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,14 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import PublicLayout from './layouts/PublicLayout';
 import Home from './pages/Home';
+import Carousel from './components/Carousel/Carousel';
 
 function App() {
   return (
     <>
       <Router>
         <Routes>
-          <Route path="/" element={<PublicLayout> <Home/> </PublicLayout>} />
+          <Route path="/" element={<PublicLayout> <Carousel/> </PublicLayout>} />
         </Routes>
       </Router>
     </>

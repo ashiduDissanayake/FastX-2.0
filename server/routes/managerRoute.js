4aa7775d2978fc12a0a2c308dba4defb4256919b
Pathumi Ranasinghe <162596287+PathumiRanasinghe@users.noreply.github.com>
2024-10-22 09:25:14 +0530

    update 2

--- server/routes/managerRoute.js
+++ server/routes/managerRoute.js
@@ -14,7 +14,6 @@ router.get('/getdriverassistant/:storeId', managerController.getDriverAssistant)
 router.get('/gettruck/:storeId', managerController.getTruck);     
 router.get('/getstore', managerController.getStore);     
 router.get('/getroute/:storeId', managerController.getRoute);   
-// router.get('/getscheduletrip', managerController.getScheduleTrip); 
 router.get('/getstoreorders/:storeId', managerController.getStoreOrders);
 
 router.get('/getactivetrips/:storeId', managerController.getActiveTripsByStore);

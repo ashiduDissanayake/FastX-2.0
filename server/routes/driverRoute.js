eb71b30a2ec4e2bc6e740a42038bbe8e634f32e9
Ashidu Dissanayake <157984013+ashiduDissanayake@users.noreply.github.com>
2024-10-30 01:37:33 +0530

    Updated driver

--- server/routes/driverRoute.js
+++ server/routes/driverRoute.js
@@ -7,8 +7,8 @@ router.post('/driverlogin', driverController.driverLogin);
 router.post('/logout', driverController.logoutDriver);      // GET /user/logout - logout the user
 router.get('/check-auth', driverController.checkAuth);    
 router.get('/order-deliver', DriverauthenticateToken, driverController.getDriverorders);
-router.patch('/deliver/:order_id', driverController.deliverOrder);
-router.get('/truck-schedules', driverController.getAllSchedules);
+router.patch('/deliver/:order_id',DriverauthenticateToken, driverController.deliverOrder);
+router.get('/truck-schedules',DriverauthenticateToken, driverController.getAllSchedules);
 router.get('/profile', DriverauthenticateToken, driverController.getDriverDetails);
 
 module.exports = router;
